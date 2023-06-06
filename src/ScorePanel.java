a271e34a1dc860283500ef11e7ae41a7b0baee84
Isaac Reynolds <73602312+Sherlock1836@users.noreply.github.com>
2023-06-05 20:59:11 -0500

    added current score display

--- src/ScorePanel.java
+++ src/ScorePanel.java
@@ -1,22 +1,27 @@
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.*;
 
 import javax.swing.JComponent;
 
 public class ScorePanel extends JComponent{
     public ScorePanel() {
-        setBackground(Color.DARK_GRAY);
+        setBackground(new Color(12, 21, 105));
     }
 
     public void paintComponent(Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
         g2D.setColor(getBackground());
         g2D.fillRect(0, 0, getWidth(), getHeight());
-
+        drawScore(g2D);
     }
     public void drawScore(Graphics2D g2D) {
-        
+        String txt = "Score: " + App.getScore() + "  ";
+        g2D.setColor(Color.BLACK);
+        g2D.setFont(new Font("Arial Black", Font.PLAIN, 24));
+        FontMetrics fm = g2D.getFontMetrics();
+        int txtWidth = fm.stringWidth(txt);
+        int txtHeight = fm.getHeight();
+        g2D.drawString(txt, getWidth() - txtWidth, txtHeight);
     }
     public void updatePanel() {
         repaint();
